1164620a46f9b1ff880e8c093d96dac3967ac6c5
WestfW <westfw@gmail.com>
2020-09-28 23:31:06 -0700

    Add additional macros for Serial using explicit pins/pads Add initial I2C support for SAM51

--- test_sercomlib/sercomlib.h
+++ test_sercomlib/sercomlib.h
@@ -2,10 +2,11 @@
  * sercomlib.h
  * July 2020, by Bill Westfield
  * Released to the public domain.
- * 
- * This defines macros to pair up "un-used" sercom interfaces with Arduino driver-level
- *  definitions like UART/HardwareSerial, on SAMD51-based boards.
- *  
+ *
+ * This defines macros to pair up "un-used" sercom interfaces with
+ *  Arduino driver-level definitions like UART/HardwareSerial, on
+ *  SAMD51-based boards.
+ *
  *  Use it like:
  *     sercom_UseSerial(sercomNumber, SerialNumber)
  *  It's all relatively ugly pre-processor magic, and it
@@ -14,14 +15,68 @@
  *  are already defined (True for Adafruit's Grand Central Station board,
  *  but not for some others.
  *  SerialN and the SERCOMn_x_Handler() end up being defined.
- *     
+ *
  *  At some point, this might get expanded to allow I2C and SPI as well,
  *  and support other chip families.
  */
+
 #include <variant.h>
+
+#if SAMD51_SERIES
+// Use this form where there are pre-defined PIN_SERIALn_RX and
+//   PIN_SERIALn_TX symbols
 #define sercom_UseSerial(_sercom, _serialNum) \
-  Uart Serial##_serialNum( &sercom##_sercom, PIN_SERIAL##_serialNum##_RX, PIN_SERIAL##_serialNum##_TX, SERCOM_RX_PAD_1, UART_TX_PAD_0 ) ; \
+  Uart Serial##_serialNum( &sercom##_sercom, PIN_SERIAL##_serialNum##_RX, \
+                           PIN_SERIAL##_serialNum##_TX,                   \
+                           SERCOM_RX_PAD_1, UART_TX_PAD_0 ) ;             \
   void SERCOM##_sercom##_0_Handler(void) {  Serial##_serialNum.IrqHandler(); } \
   void SERCOM##_sercom##_1_Handler(void) {  Serial##_serialNum.IrqHandler(); } \
   void SERCOM##_sercom##_2_Handler(void) {  Serial##_serialNum.IrqHandler(); } \
   void SERCOM##_sercom##_3_Handler(void) {  Serial##_serialNum.IrqHandler(); }
+
+// Use this form to specify the TX and RX pin numbers (Arduino Pin numbers.)
+#define sercom_UseSerialPins(_sercom, _ser, _rx, _tx) \
+  Uart _ser( &sercom##_sercom, _rx, _tx, SERCOM_RX_PAD_1, UART_TX_PAD_0 ) ; \
+  void SERCOM##_sercom##_0_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_1_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_2_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_3_Handler(void) {  _ser.IrqHandler(); }
+
+// Use this form to specify the TX and RX pin numbers AND PAD designators
+//  Note that on SAMD51, the TX pad is fixed.
+#define sercom_UseSerialPinsPads(_sercom, _serialNum, _rx, _tx, _rxpad, _txpad) \
+  Uart _ser( &sercom##_sercom, _rx, _tx, _txpad, _txpad ) ; \
+  void SERCOM##_sercom##_0_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_1_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_2_Handler(void) {  _ser.IrqHandler(); } \
+  void SERCOM##_sercom##_3_Handler(void) {  _ser.IrqHandler(); }
+
+// Produce TwoWire name(&sercomN, PIN_sda, PIN_scl)
+  #define sercom_UseI2CPins(_sercom, _i2c, _sda, _scl) \
+  TwoWire _i2c(&sercom##_sercom, _sda, _scl); \
+  void SERCOM##_sercom##_0_Handler(void) {  _i2c.onService(); } \
+  void SERCOM##_sercom##_1_Handler(void) {  _i2c.onService(); } \
+  void SERCOM##_sercom##_2_Handler(void) {  _i2c.onService(); } \
+  void SERCOM##_sercom##_3_Handler(void) {  _i2c.onService(); }
+#endif // SAMD51
+
+#if SAMD21_SERIES || SAMD10_SERIES || SAMD11_SERIES
+
+// Use this form where there are pre-defined PIN_SERIALn_RX and
+//  PIN_SERIALn_TX symbols
+#define sercom_UseSerial(_sercom, _serialNum) \
+  Uart _ser( &sercom##_sercom, PIN_Serial##_serialNum##_RX,  \
+             PIN_SERIAL##_serialNum##_TX,                    \
+             SERCOM_RX_PAD_1, UART_TX_PAD_0 ) ;              \
+  void SERCOM##_sercom##_Handler(void) {  _ser.IrqHandler(); }
+
+// Use this form to specify the TX and RX pin numbers (Arduino Pin numbers.)
+#define sercom_UseSerialPins(_sercom, _serialNum, _rx, _tx) \
+  Uart _ser( &sercom##_sercom, _rx, _tx, SERCOM_RX_PAD_1, UART_TX_PAD_0 ) ; \
+  void SERCOM##_sercom##_Handler(void) {  _ser.IrqHandler(); }
+
+// Use this form to specify the TX and RX pin numbers AND PAD designators
+#define sercom_UseSerialPinsPads(_sercom, _serialNum, _rx, _tx, _rxpad, _txpad) \
+  Uart _ser( &sercom##_sercom, _rx, _tx, _txpad, _txpad ) ; \
+  void SERCOM##_sercom##_Handler(void) {  _ser.IrqHandler(); }
+#endif // SAMD21
--- test_sercomlib/test_sercomlib.ino
+++ test_sercomlib/test_sercomlib.ino
@@ -1,52 +1,96 @@
 #include "sercomlib.h"
 /*
- *  July 2020 by Bill Westfield.  Released to the public domain.
+ *    July 2020 by Bill Westfield.  Released to the public domain.
+ *
+ *   Attach the extra SERCOM peripherals to Serial Ports (UART mode.)
+ *   Note that for Grand Central, the required PIN_SERIALn_RX/etc 
+ *    defines are already in variant.h
+ *   For other boards, it may be necessary to define them.
  */
-/*
- *  Attach the extra SERCOM peripherals to Serial Ports (UART mode.)
- *  Note that for Grand Central, the required PIN_SERIALn_RX/etc defines are already in variant.h
- *  For other boards, it may be necessary to define them.
- */
+
+
+//#define DEMO_I2C 1
+
 //              SERCOM#, Serial#
 sercom_UseSerial(  4,      2)   // Serial2 - pins 18 and 19
 sercom_UseSerial(  1,      3)   // Serial3 - pins 16 and 17
 sercom_UseSerial(  5,      4)   // Serial4 - pins 14 and 15
+#ifdef DEMO_I2C
+#include <Wire.h>
+sercom_UseI2CPins( 7,   Wire7, 82, 81)  // Wire7 on SPI pins.
+#else
+sercom_UseSerialPins(3, SerialI2C, 21, 20)  // SerialI2C - I2C pins
+#endif
 
+void pullup (int pin) {
+  PortGroup *port = digitalPinToPort(pin);
+  port->OUTSET.reg = g_APinDescription[pin].ulPin ;
+  port->PINCFG[g_APinDescription[pin].ulPin].reg |= PORT_PINCFG_PULLEN ;
+}
 
 void setup() {
   // Start ports at different speeds to demo their independence.
   Serial.begin(9600);
-  Serial1.begin(19200);     // Serial1 (pins 0 and 1) alread defined.
-  Serial2.begin(38400);
-  Serial3.begin(115200);
+
+  Serial1.begin(9600);     // Serial1 (pins 0 and 1) alread defined.
+  pullup(PIN_SERIAL1_RX);
+
+  Serial2.begin(9600);
+  pullup(PIN_SERIAL2_RX);
+
+  Serial3.begin(9600);
+  pullup(PIN_SERIAL3_RX);
+
   Serial4.begin(9600);
+  pullup(PIN_SERIAL4_RX);
+
+#ifdef DEMO_I2C
+  Wire7.begin();
+#else
+  SerialI2C.begin(9600);
+  pullup(21);
+#endif
+  
   while (!Serial)           // Wait for USB Serial to finish initialized
     ;
 }
 
 void loop() {
   Serial.println("This is Serial");
-  
+  Serial.println("1");
   Serial1.println("This a Serial 1");    // Check Transmit
   while (Serial1.available()) {          // Check receive
     Serial1.print("Serial 1 read ");
     Serial1.println(Serial1.read());
   }
+  Serial.println("2");
   Serial2.println("This a Serial 2");
   while (Serial2.available()) {
     Serial2.print("Serial 2 read ");
     Serial2.println(Serial2.read());
   }
+  Serial.println("3");
+
   Serial3.println("This a Serial 3");
   while (Serial3.available()) {
     Serial3.print("Serial 3 read ");
     Serial3.println(Serial3.read());
   }
+  Serial.println("4");
+
   Serial4.println("This a Serial 4");
   while (Serial4.available()) {
     Serial4.print("Serial 4 read ");
     Serial4.println(Serial4.read());
   }
+#ifndef DEMO_I2C
+  Serial.println("I2C");
+  SerialI2C.println("This a Serial I2C");
+  while (SerialI2C.available()) {
+    SerialI2C.print("Serial I2C read ");
+    SerialI2C.println(SerialI2C.read());
+  }
+#endif
   delay(1000);
 
 }
