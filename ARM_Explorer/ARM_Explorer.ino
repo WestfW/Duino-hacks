f687c92e25986bcc3a68e400234cfb99d7699c1e
Bill Westfield <westfw@gmail.com>
2023-08-25 17:33:04 -0700

    Update ARM_Explorer.ino
    Add MPU, EMACS Macro for processing .h structure info

--- ARM_Explorer/ARM_Explorer.ino
+++ ARM_Explorer/ARM_Explorer.ino
@@ -87,9 +87,11 @@ void backgroundMonitor() {
     }
 #endif
     static const char *cmd_strings =
-      PSTR("board ""bytes ""hwords "   "words " "pwm " "interrupts " "Serial "   "pins " "scb " "fpu " "systick "   "help "    "? ");
+      PSTR("board ""bytes ""hwords "   "words " "pwm " "interrupts " "Serial "   "pins "
+           "scb " "fpu " "mpu " "systick "   "help "    "? ");
     enum {
-      CMD_BOARD, CMD_BYTE, CMD_HWORD, CMD_WORD, CMD_PWM, CMD_INT,    CMD_SERCOM, CMD_PINS, CMD_SCB, CMD_FPU, CMD_SYSTICK, CMD_HELP, CMD_HELP2  // make sure this matches the string
+      CMD_BOARD, CMD_BYTE, CMD_HWORD, CMD_WORD, CMD_PWM, CMD_INT,    CMD_SERCOM, CMD_PINS,
+      CMD_SCB, CMD_FPU, CMD_MPU, CMD_SYSTICK, CMD_HELP, CMD_HELP2  // make sure this matches the string
     };
 
     cmd = ttycli.keyword(cmd_strings); // look for a command.
@@ -127,18 +129,22 @@ void backgroundMonitor() {
         print_systick();
         break;
 
+      case CMD_MPU:
+        print_mpu();
+        break;
+
       case CMD_INT:
         {
           int nInts;
 #ifdef SCnSCB //v7m has data about interrupts
-          sprintf(spbuffer, "  SCnSCB->ICTR= %08lx (%s)",
+          sprintf(spbuffer, "  SCnSCB->ICTR= %08lX (%s)",
                   SCnSCB->ICTR, "Interrupt Controller info");
           mySerial.print(spbuffer);
           int field = ((SCnSCB->ICTR & SCnSCB_ICTR_INTLINESNUM_Msk) >> SCnSCB_ICTR_INTLINESNUM_Pos);
           nInts = (field + 1) * 32;
           sprintf(spbuffer, " (%ld extern Interrputs)\n", nInts);
           mySerial.print(spbuffer);
-          sprintf(spbuffer, "  SCnSCB->ACTLR= %08lx (%s)\n", SCnSCB->ACTLR, "Auxiliary Control info");
+          sprintf(spbuffer, "  SCnSCB->ACTLR= %08lX (%s)\n", SCnSCB->ACTLR, "Auxiliary Control info");
           mySerial.print(spbuffer);
 #else //v6m has 32 external interrupts.
           nInts = 32;
@@ -220,7 +226,7 @@ void backgroundMonitor() {
           } else break;
 
           while (n > 0) {
-            sprintf(spbuffer, "0x%08lx : ", (uint32_t)p);
+            sprintf(spbuffer, "0x%08lX : ", (uint32_t)p);
             Serial.print(spbuffer);
             for (int i = 0; i < 16; i++) {
               sprintf(spbuffer, " %02x", p[i]);
@@ -246,7 +252,7 @@ void backgroundMonitor() {
           } else break;
 
           while (n > 0) {
-            sprintf(spbuffer, "0x%08lx : ", (uint32_t)p);
+            sprintf(spbuffer, "0x%08lX : ", (uint32_t)p);
             Serial.print(spbuffer);
             for (int i = 0; i < 16; i++) {
               sprintf(spbuffer, " %04lx", p[i]);
@@ -272,10 +278,10 @@ void backgroundMonitor() {
           } else break;
 
           while (n > 0) {
-            sprintf(spbuffer, "0x%08lx : ", (uint32_t)p);
+            sprintf(spbuffer, "0x%08lX : ", (uint32_t)p);
             Serial.print(spbuffer);
             for (int i = 0; i < 8; i++) {
-              sprintf(spbuffer, " %08lx", p[i]);
+              sprintf(spbuffer, " %08lX", p[i]);
               Serial.print(spbuffer);
             }
             Serial.println();
@@ -304,45 +310,45 @@ void backgroundMonitor() {
 void print_scb() {
   SCB_Type *p = SCB;
   mySerial.print("SCB (System Control Block @0x"); mySerial.println((uint32_t)SCB, HEX);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "CPUID", p->CPUID, " CPUID Base Register \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "CPUID", p->CPUID, " CPUID Base Register \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "ICSR", p->ICSR, " Interrupt Control and State  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "ICSR", p->ICSR, " Interrupt Control and State  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "VTOR", p->VTOR, " Vector Table Offset  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "VTOR", p->VTOR, " Vector Table Offset  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "AIRCR", p->AIRCR, " Application Interrupt and Reset Control  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "AIRCR", p->AIRCR, " Application Interrupt and Reset Control  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "SCR", p->SCR, " System Control  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "SCR", p->SCR, " System Control  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "CCR", p->CCR, " Configuration Control  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "CCR", p->CCR, " Configuration Control  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s",  "SHP", p->SHP[0], " System Handlers Priority Registers (4-7, 8-11, 12-15) \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s",  "SHP", p->SHP[0], " System Handlers Priority Registers (4-7, 8-11, 12-15) \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "SHCSR", p->SHCSR, " System Handler Control and State  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "SHCSR", p->SHCSR, " System Handler Control and State  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "CFSR", p->CFSR, " Configurable Fault Status  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "CFSR", p->CFSR, " Configurable Fault Status  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "HFSR", p->HFSR, " HardFault Status  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "HFSR", p->HFSR, " HardFault Status  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "DFSR", p->DFSR, " Debug Fault Status  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "DFSR", p->DFSR, " Debug Fault Status  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "MMFAR", p->MMFAR, " MemManage Fault Address  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "MMFAR", p->MMFAR, " MemManage Fault Address  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "BFAR", p->BFAR, " BusFault Address  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "BFAR", p->BFAR, " BusFault Address  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "AFSR", p->AFSR, " Auxiliary Fault Status  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "AFSR", p->AFSR, " Auxiliary Fault Status  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "PFR", p->PFR[0], " Processor Feature  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "PFR", p->PFR[0], " Processor Feature  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "DFR", p->DFR, " Debug Feature  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "DFR", p->DFR, " Debug Feature  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "ADR", p->ADR, " Auxiliary Feature  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "ADR", p->ADR, " Auxiliary Feature  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "MMFR", p->MMFR[0], " Memory Model Feature  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "MMFR", p->MMFR[0], " Memory Model Feature  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "ISAR", p->ISAR[0], " Instruction Set Attributes  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "ISAR", p->ISAR[0], " Instruction Set Attributes  \n");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s", "CPACR", p->CPACR, " Coprocessor Access Control  \n");
+  sprintf(spbuffer, "%6s=0x%08lX%s", "CPACR", p->CPACR, " Coprocessor Access Control  \n");
   mySerial.print(spbuffer);
 }
 
@@ -351,21 +357,21 @@ void print_fpu() {
   mySerial.println("No Floating Point is present.");
 #else
   FPU_Type *p = FPU;
-  sprintf(spbuffer, "FPU (Float Processing Unit info @%08lx)\n", (uint32_t)FPU);
+  sprintf(spbuffer, "FPU (Float Processing Unit info @%08lX)\n", (uint32_t)FPU);
   mySerial.print(spbuffer);
 
-  sprintf(spbuffer, "  %6s=0x%08lx%s\n", "FPCCR", p->FPCCR, " FP Context Control ");
-  if (p->FPCCR & FPU_FPCCR_LSPACT_Msk) {
+  sprintf(spbuffer, "  %6s=0x%08lX%s\n", "FPCCR", p->FPCCR, " FP Context Control ");
+  if (p->FPCCR & FPU_FPCCR_LSPEN_Msk) {
     mySerial.print("    (Lazy State Preservation is ON)\n");
   }
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "  %6s=0x%08lx%s\n", "FPCAR", p->FPCAR, " FP Context Address ");
+  sprintf(spbuffer, "  %6s=0x%08lX%s\n", "FPCAR", p->FPCAR, " FP Context Address ");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "  %6s=0x%08lx%s\n", "FPDSCR", p->FPDSCR, " FP Default Status Control ");
+  sprintf(spbuffer, "  %6s=0x%08lX%s\n", "FPDSCR", p->FPDSCR, " FP Default Status Control ");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "  %6s=0x%08lx%s\n", "MVFR0", p->MVFR0, " Media and FP Feature  0");
+  sprintf(spbuffer, "  %6s=0x%08lX%s\n", "MVFR0", p->MVFR0, " Media and FP Feature  0");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "  %6s=0x%08lx%s\n", "MVFR1", p->MVFR1, " Media and FP Feature  1");
+  sprintf(spbuffer, "  %6s=0x%08lX%s\n", "MVFR1", p->MVFR1, " Media and FP Feature  1");
   mySerial.print(spbuffer);
 #endif
 }
@@ -373,13 +379,98 @@ void print_fpu() {
 void print_systick() {
   SysTick_Type *p = SysTick;
   Serial.println("SysTick Registers");
-  sprintf(spbuffer, "%6s=0x%08lx%s\n", "CTRL", p-> CTRL, " Control and Status ");
+  sprintf(spbuffer, "%6s=0x%08lX%s\n", "CTRL", p-> CTRL, " Control and Status ");
   mySerial.print(spbuffer);
   sprintf(spbuffer, "%6s=%8ld%s\n", "LOAD", p-> LOAD, " Reload Value ");
   mySerial.print(spbuffer);
   sprintf(spbuffer, "%6s=%8ld%s\n", "VAL", p-> VAL, " Current Value ");
   mySerial.print(spbuffer);
-  sprintf(spbuffer, "%6s=0x%08lx%s\n", "CALIB", p-> CALIB, " Calibration ");
+  sprintf(spbuffer, "%6s=0x%08lX%s\n", "CALIB", p-> CALIB, " Calibration ");
   mySerial.print(spbuffer);
 }
 
+void print_mpu() {
+#if __MPU_PRESENT
+  mySerial.println("Memory Protection Unit Registers");
+  MPU_Type *p = MPU;
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "TYPE", p->TYPE, "MPU Type");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "CTRL", p->CTRL, "MPU Control");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RNR", p->RNR, "MPU Region RNRber");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RBAR", p->RBAR, "MPU Region Base Address");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RASR", p->RASR, "MPU Region Attribute and Size");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RBAR_A1", p->RBAR_A1, "MPU Alias 1 Region Base Address");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RASR_A1", p->RASR_A1, "MPU Alias 1 Region Attribute and Size");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RBAR_A2", p->RBAR_A2, "MPU Alias 2 Region Base Address");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RASR_A2", p->RASR_A2, "MPU Alias 2 Region Attribute and Size");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RBAR_A3", p->RBAR_A3, "MPU Alias 3 Region Base Address");
+  mySerial.print(spbuffer);
+  sprintf(spbuffer, "  %6s=%0x08lx (%s)", "RASR_A3", p->RASR_A3, "MPU Alias 3 Region Attribute and Size");
+  mySerial.print(spbuffer);
+#else
+  mySerial.println("No MPU present");
+#endif
+}
+/* EMACS Keyboard macro to help turn .h files into descriptive printfs.
+  ;; Keyboard Macro Editor.  Press C-c C-c to finish; press C-x k RET to cancel.
+  ;; Original keys: ESC d ESC d ESC d sprintf ( spbuffer , "%6s=%0x08lx SPC ( %s ) " , " SPC C-f ESC \ ESC C-s ; C-b C-w C-d C-y " , SPC p-> C-y , SPC " M-\ C-e " ) ; C-n C-a mySerial.print ( spbuffer ) ; RET
+
+  Command: last-kbd-macro
+  Key: none
+
+  Macro:
+
+  ESC d      ;; kill-word
+  ESC d     ;; kill-word
+  ESC d     ;; kill-word
+  sprintf     ;; self-insert-command * 7
+  (     ;; c-electric-paren
+  spbuffer    ;; self-insert-command * 8
+  ,     ;; c-electric-semi&comma
+  "%6s=%0x08lx    ;; self-insert-command * 12
+  SPC     ;; self-insert-command
+  (     ;; c-electric-paren
+  %s      ;; self-insert-command * 2
+  )     ;; c-electric-paren
+  "     ;; self-insert-command
+  ,     ;; c-electric-semi&comma
+  "     ;; self-insert-command
+  SPC     ;; self-insert-command
+  C-f     ;; forward-char
+  ESC \     ;; delete-horizontal-space
+  ESC C-s     ;; isearch-forward-regexp
+  ;     ;; c-electric-semi&comma
+  C-b     ;; backward-char
+  C-w     ;; kill-region
+  C-d     ;; c-electric-delete-forward
+  C-y     ;; yank
+  "     ;; self-insert-command
+  ,     ;; c-electric-semi&comma
+  SPC     ;; self-insert-command
+  p->     ;; self-insert-command * 3
+  C-y     ;; yank
+  ,     ;; c-electric-semi&comma
+  SPC     ;; self-insert-command
+  "     ;; self-insert-command
+  M-\     ;; delete-horizontal-space
+  C-e     ;; move-end-of-line
+  "     ;; self-insert-command
+  )     ;; c-electric-paren
+  ;     ;; c-electric-semi&comma
+  C-n     ;; next-line
+  C-a     ;; move-beginning-of-line
+  mySerial.print    ;; self-insert-command * 14
+  (     ;; c-electric-paren
+  spbuffer    ;; self-insert-command * 8
+  )     ;; c-electric-paren
+  ;     ;; c-electric-semi&comma
+  RET     ;; newline
+*/
